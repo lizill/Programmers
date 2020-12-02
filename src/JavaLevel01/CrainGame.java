e8fe2f3b91d41b21628e4a1f53183ab9ff37b322
Park-Donghyeon <68741593+Park-Donghyeon@users.noreply.github.com>
2020-12-02 22:50:04 +0900

    carin

--- src/JavaLevel01/CrainGame.java
+++ src/JavaLevel01/CrainGame.java
@@ -23,49 +23,6 @@ public class CrainGame {
 	         * 4 3 1 1 3 2 0 4
 	         */
 	        
-	        int[] tmp = new int[moves.length];
-	        for(int i=0; i<tmp.length; i++) {
-	        	tmp[i]=0;
-	        }
-	        
-	        boolean zero = false;
-	        for(int i=0; i<moves.length; i++) {
-	        	for(int j=0; j<board.length; j++) {
-	        		if(board[j][moves[i]-1] != 0) {
-	        			tmp[i] = board[j][moves[i]-1];
-	        			board[j][moves[i]-1] = 0;
-	        			zero = false;
-	        			break;
-	        		}
-	        	}
-	        	if(zero) {
-	        		tmp[i] = 0;
-	        	}
-	        }
-	        
-	        System.out.println(Arrays.toString(tmp));
-	        
-	        int[] tmp2 = new int[tmp.length];
-	        int count = 0;
-	        
-	        for(int i=0; i<tmp.length; i++) { // tmp2의 첫번째 자리를 입력
-	        	if(tmp[i] != 0) tmp2[0] = tmp[i];
-	        }
-	        
-	        for(int i=1; i<tmp2.length; i++) {
-	        	for(int j=1; j<tmp.length; j++) {
-	        		if(tmp[j] != tmp[i-1] && tmp[j] != 0) {
-	        			tmp2[i] = tmp[j];
-	        		} else if (tmp[j] == 0) {
-	        			continue;
-	        		} else if (tmp[j] == tmp[i-1]) {
-	        			count++;
-//	        			i--;
-	        			break;
-	        		}
-	        	}
-	        }
-	        
-	        System.out.println(count);
+		
 	}
 }
